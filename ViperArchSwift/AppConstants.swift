0bfc0a96fe478b15ce97ec7d6f335703c14eb057
jeetrajput01 <51108910+jeetrajput01@users.noreply.github.com>
2024-04-30 14:42:26 +0530

    Update AppConstants.swift

--- ViperArchSwift/AppConstants.swift
+++ ViperArchSwift/AppConstants.swift
@@ -10,7 +10,7 @@ import Foundation
 
 enum apiURLs {
     
-    static let apiKey  = "65c89ea0f6e57759384ae632"
+    static let apiKey  = ""
     static let baseURL = "https://dummyapi.io/data/v1/"
     static let userList =  baseURL + "user?limit=10"
     static let userData = baseURL + "user/"
